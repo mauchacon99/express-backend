cc41fd558f82b129b56548616da6f1fdfd525062
arturoluna879 <arturoluna879@gmail.com>
2021-09-29 12:43:27 -0400

    migrate mysql to mariadb and fix errors

--- config/database.js
+++ config/database.js
@@ -1,10 +1,16 @@
+require('dotenv-safe').config({
+    allowEmptyValues: true
+})
+
 module.exports = {
     // config db
     username: process.env.DB_USERNAME || "root",
     password: process.env.DB_PASSWORD || null,
     database: process.env.DB_DATABASE || "database_development",
-    host: process.env.DB_HOST || "127.0.0.1",
-    dialect: "mysql",
+    dialect: "mariadb",
+    dialectOptions: {
+        host: process.env.DB_HOST || "127.0.0.1",
+    },
 
     // config seeds
     seederStorage: 'sequelize',
