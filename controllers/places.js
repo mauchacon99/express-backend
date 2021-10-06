c6d63c05c429e0bb76c1a1ac679494e9661f5843
arturoluna879 <arturoluna879@gmail.com>
2021-10-06 10:49:06 -0400

    google places

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,7 @@
 const { matchedData } = require('express-validator')
+const axios = require('axios');
 const utils = require("../middleware/utils");
+const apiKey = process.env.API_KEY_GOOGLE
 
 /**
  * Get item function called by route
@@ -8,9 +10,17 @@ const utils = require("../middleware/utils");
  */
 exports.getPlace = async (req, res) => {
     try {
-        req = matchedData(req)
-        res.status(200).json({msg: req})
+        const { input } = matchedData(req)
+        const config = {
+            method: 'get',
+            url: `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&types=establishment&key=${apiKey}`,
+            headers: { 'Content-Type': 'application/json' }
+        };
+
+        axios(config)
+            .then(({ data }) => res.status(200).json(data))
+            .catch(() => utils.handleError(res, utils.buildErrObject(404, 'NOT_FOUND_PLACE')))
     } catch (error) {
-        utils.handleError(res, error)
+        utils.handleError(res, utils.buildErrObject(404, 'NOT_FOUND_PLACE'))
     }
 }
