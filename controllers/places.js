b2327563e362fbefe66b6ce4ad2ffda6cab48fb7
arturoluna879 <arturoluna879@gmail.com>
2021-10-19 19:52:10 -0400

    fix places

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,17 @@
 const { matchedData } = require('express-validator')
-const axios = require('axios');
 const utils = require("../middleware/utils");
 const apiKey = process.env.API_KEY_GOOGLE
+const GooglePlaces = require('google-places');
+const places = new GooglePlaces(apiKey);
 
+const getDetail = (a) => {
+    return new Promise((resolve, reject) => {
+        places.details({reference: a.reference}, (err, res) => {
+            if(err) reject(err)
+                else resolve(res)
+        });
+    })
+}
 /**
  * Get item function called by route
  * @param {Object} req - request object
@@ -11,16 +20,20 @@ const apiKey = process.env.API_KEY_GOOGLE
 exports.getPlace = async (req, res) => {
     try {
         const { input } = matchedData(req)
-        const config = {
-            method: 'get',
-            url: `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&types=establishment&key=${apiKey}`,
-            headers: { 'Content-Type': 'application/json' }
-        };
-
-        axios(config)
-            .then(({ data }) => res.status(200).json(data))
-            .catch(() => utils.handleError(res, utils.buildErrObject(404, 'NOT_FOUND_PLACE')))
+        let data = []
+        places.autocomplete({input, types: "(cities)"}, async (err, response) => {
+            if (err)
+                utils.handleError(res, utils.buildErrObject(404, 'NOT_FOUND_PLACE'))
+            else {
+                for(const index in response.predictions) {
+                    data.push(await getDetail(response.predictions[index]))
+                }
+                // response.predictions.map(async (a) => )
+                res.status(200).json(data)
+            }
+        })
     } catch (error) {
+        console.log(error)
         utils.handleError(res, utils.buildErrObject(404, 'NOT_FOUND_PLACE'))
     }
 }
